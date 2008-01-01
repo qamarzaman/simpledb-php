fda385008456d8f45393130662b42320cbfd525b
ejh237 <ejh237@b324de93-8a42-0410-914a-71b7aac83a78>
2008-01-01 04:13:06 +0000

    oops, uncommented the constants you will be changing the values of.  NOTE: You have to rename this file "awsconst.php", but I guess you would have figured that out.

--- sdbrowser/awsconst.default.php
+++ sdbrowser/awsconst.default.php
@@ -9,6 +9,9 @@
  * 
  * Right now this is just a place to get the private keys out fo the code
  *
+ * USAGE: I guess it is obvious, but change this filename to "awsconst.php" and
+ * set it to be ignored by your SVN client (if you use one). THEN, put your 
+ * private keys in your private copy.
  * 
  * VERSION	DATE		WHO		COMMENTS
  * 0.1		12/31/07	ejh		Initial version, a bit distracted watching Oregon play
@@ -16,8 +19,8 @@
  */
 
 class awsconst {
-//	static public $kAccessKeyID = "[YOUR ACCESS KEY ID HERE]";
-//	static public $kSecretAccessKey = "[YOUR SECRET ACCESS KEY HERE]";
+	static public $kAccessKeyID = "[YOUR ACCESS KEY ID HERE]";
+	static public $kSecretAccessKey = "[YOUR SECRET ACCESS KEY HERE]";
 };
 
 ?>
